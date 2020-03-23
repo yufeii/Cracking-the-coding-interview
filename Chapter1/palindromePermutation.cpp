8c03e7d99fb330ca68d24a5b471f4afaaff440bc
yufeii <smileyufy@163.com>
2020-03-23 21:15:55 +0100

    Update palindromePermutation.cpp

--- Chapter1/palindromePermutation.cpp
+++ Chapter1/palindromePermutation.cpp
@@ -1,1 +1,13 @@
+/*
+Problem Statement :
+Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or
+phrase that is the same forwards and backwards. A permutation is a rearrangement of letters.The palindrome does
+not need to be limited to just dictionary words.
+Example :
+	"tact coa"->true(permutations : "tacocat", "atcocta", etc.)
+*/
 
+bool isPermutationOfPallindrome1( string& str )
+{
+  
+}
