16ef39aaf8635d37014051f8450dda27feb350ff
yufeii <smileyufy@163.com>
2020-03-23 21:54:48 +0100

    Update String_Rotate.cpp

--- Chapter1/String_Rotate.cpp
+++ Chapter1/String_Rotate.cpp
@@ -1,1 +1,8 @@
-
+/*
+Problem Statement:
+Assume you have a method isSubstring() which checks if one word is a substring of
+another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using
+only one call to isSubstring().
+Example:
+stringRotation("waterbottle", "erbottlewat")
+*/
